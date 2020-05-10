c85cfa209279000ca7c787c49784f32a8425c0bb
FelixCoderArt <falkovskyfelix@gmail.com>
2020-05-10 17:26:27 +0300

    Initial Commit

---
+++ CustomMultiPickerSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomMultiPickerSwiftUI
+//
+//  Created by Felix Falkovsky on 10.05.2020.
+//  Copyright © 2020 Felix Falkovsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
